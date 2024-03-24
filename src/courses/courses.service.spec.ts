b4eccb91ad5e685eb121c60930e687dde06a90e5
gwonyeong <qwe980121@naver.com>
2024-03-24 21:32:03 +0900

    feat: [apply] 신청 기록 생성을 호출해야함

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoursesService } from './courses.service';
 import { CoursesRepositoryPort } from './port/courses.repository.interface';
+import { CoursesUserRepositoryPort } from './port/courses-user.repository.port';
 
 describe('CoursesService', () => {
   let service: CoursesService;
   let coursesRepositoryPort: CoursesRepositoryPort;
+  let coursesUserRepository: CoursesUserRepositoryPort = {
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
-    service = new CoursesService(coursesRepositoryPort);
+    service = new CoursesService(coursesRepositoryPort, coursesUserRepository);
   });
 
   it('should be defined', () => {
@@ -16,6 +21,15 @@ describe('CoursesService', () => {
   describe('apply', () => {
     it('존재해야 한다.', () => {
       expect(service.apply).toBeDefined();
+    });
+
+    it('신청 기록 생성을 호출해야한다.', async () => {
+      const courseId = 1;
+      const userId = 1;
+
+      await service.apply({ courseId, userId });
+
+      expect(coursesUserRepository.create).toHaveBeenCalled();
     });
   });
 });
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CoursesRepositoryPort } from './port/courses.repository.interface';
+import { CoursesUserRepositoryPort } from './port/courses-user.repository.port';
+import { Course, User } from '@prisma/client';
 
 @Injectable()
 export class CoursesService {
-  constructor(private readonly coursesRepository: CoursesRepositoryPort) {}
+  constructor(
+    private readonly coursesRepository: CoursesRepositoryPort,
+    private readonly coursesUserRepository: CoursesUserRepositoryPort,
+  ) {}
 
   async getOne({ courseId }: { courseId: number }) {}
 
-  async apply({ course, user }) {}
+  async apply({ courseId, userId }: { courseId: number; userId: number }) {
+    await this.coursesUserRepository.create({
+      courseId,
+      userId,
+    });
+  }
 }
